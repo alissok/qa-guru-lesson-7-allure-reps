e38497f821a13badfbe2180737939f0add34fe25
Alisa.Kadilenko <alisa.kadilenko@nexign.com>
2022-04-22 00:20:43 +0300

    Allure Homework

--- src/test/java/com/nexign/IssuesSearchTest.java
+++ src/test/java/com/nexign/IssuesSearchTest.java
@@ -1,113 +1,69 @@
 package com.nexign;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import com.nexign.filters.FilterCategories;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-@DisplayName("Класс с тестами для проверки функции поиска товаров по тексту в интернет-магазине Wildberries")
+@DisplayName("Класс с тестами для работы с Allure Reports")
 public class IssuesSearchTest {
     //Vars
-    String  url = "https://www.wildberries.ru/",
-            notFound = "По Вашему запросу ничего не найдено.";
+    String  repository = "alissok/qa-guru-lesson-5-junit",
+            url = "https://github.com",
+            issueName = "I am test issue";
+    SelenideElement repinput = $(".header-search-input");
 
-    @CsvSource(value = {
-            "Nars|Румяна",
-            "Dolce&Gabbana|Туалетная вода",
-            "MAC|Тушь",
-    },
-            delimiter = '|'
-    )
-
-    @ParameterizedTest(name = "Проверка наличия в результатах поиска категории {1} у бренда {0}")
-    void searchItemByBrand(String brand, String category) {
-        //Предусловия
-        open(url);
-        //Шаги
-        $("#searchInput").setValue(brand);
-        $("#searchInput").pressEnter();
-        //Ожидаемый результат
-        $$(".product-card__wrapper").find(text(category))
-                .shouldBe(visible)
-                .shouldNotHave(text(notFound));
-    }
-
-    static Stream<Arguments> searchItemByBrandAndCategory() {
-        return Stream.of(
-                Arguments.of("Nars", "Подушка"),
-                Arguments.of("Dolce&Gabbana", "Туалетная вода")
-        );
-    }
-
-    @MethodSource("searchItemByBrandAndCategory")
-    @ParameterizedTest(name = "Проверка наличия результатов поиска при вводе текста в формате {0} {1}")
-    void searchItemByBrandAndCategory(String brand, String category) {
+    @Test
+    void searchIssueWithLogger() {
         SelenideLogger.addListener("allure", new AllureSelenide());
-        //Предусловия
         open(url);
-        //Шаги
-        $("#searchInput").setValue(brand+ " " + category);
-        $("#searchInput").pressEnter();
-        //Ожидаемый результат
-        $$(".product-card__wrapper").find(text(category))
-                .shouldBe(visible)
-                .shouldNotHave(text(notFound));
+
+        repinput.click();
+        repinput.sendKeys(repository);
+        repinput.submit();
+
+        $(By.linkText(repository)).click();
+        $(By.partialLinkText("Issues")).click();
+        $(withText(issueName)).should(Condition.visible);
     }
 
-    @EnumSource(FilterCategories.class)
-    @ParameterizedTest()
-    void searchItemAndCheckFilters(FilterCategories testData) {
-        //Предусловия
-        open(url);
-        //Шаги
-        $("#searchInput").setValue("Nars");
-        $("#searchInput").pressEnter();
-        //Ожидаемый результат
-        $$(".catalog-page__side")
-                .find(Condition.text(testData.rusName))
-                .shouldBe(visible);
-    }
-
-    static Stream<Arguments> searchItemByBrandWb() {
-        return Stream.of(
-                Arguments.of("Nars", "Подушка"),
-                Arguments.of("Dolce&Gabbana", "Туалетная вода")
-        );
-    }
-    @MethodSource("searchItemByBrandWb")
-    @ParameterizedTest(name = "Проверка наличия результатов поиска при вводе текста в формате {0} {1}")
-    void searchItemByBrandWb(String brand, String category) {
+    @Test
+    void searchIssueWithLambdaSteps() {
         SelenideLogger.addListener("allure", new AllureSelenide());
-        //Предусловия
-        step("Открываем главную страницу", () -> {
+        step("Открываем главную страницу GitHub", () -> {
             open(url);
         });
-        //Предусловия
-        //Шаги
-        step("Ищем продукт по бренду " + brand + " и категории " + category, () -> {
-            $("#searchInput").setValue(brand+ " " + category);
+        step("Ищем репозиторий " + repository + " по ссылке", () -> {
+            repinput.click();
+            repinput.sendKeys(repository);
+            repinput.submit();
         });
-        step("Осуществляем поиск", () -> {
-            $("#searchInput").pressEnter();
+        step("Переходим по ссылке на искомый репозиторий " + repository, () -> {
+            $(By.linkText(repository)).click();
         });
-        //Ожидаемый результат
-        step("Проверяем, что поиск выдал результаты по запросу " + brand + " " + category, () -> {
-            $$(".product-card__wrapper").find(text(category))
-                    .shouldBe(visible)
-                    .shouldNotHave(text(notFound));
+        step("Переходим в раздел Issues", () -> {
+            $(By.partialLinkText("Issues")).click();
         });
+        step("Проверяем существование Issue с названием " + issueName, () -> {
+            $(withText(issueName)).should(Condition.visible);
+        });
+    }
+
+    @Test
+    void searchIssueStepsAnnotaton() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage(url);
+        steps.searchForRepository(repository);
+        steps.clickOnRepositoryLink(repository);
+        steps.openIssuesDir();
+        steps.checkIssueWithSpecificName(issueName);
     }
 }
---
+++ src/test/java/com/nexign/WebSteps.java
@@ -0,0 +1,56 @@
+package com.nexign;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.nio.charset.StandardCharsets;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class WebSteps {
+
+    @Step("Открываем главную страницу GitHub")
+    public void openMainPage(String url) {
+        open(url);
+    }
+
+    @Step("Ищем репозиторий {repository} по ссылке")
+    public void searchForRepository(String repository) {
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(repository);
+        $(".header-search-input").submit();
+    }
+
+    @Step("Переходим по ссылке на искомый репозиторий")
+    public void clickOnRepositoryLink(String repository) {
+        $(By.linkText(repository)).click();
+    }
+
+    @Step("Переходим в раздел Issues")
+    public void openIssuesDir() {
+        $(By.partialLinkText("Issues")).click();
+    }
+    @Step("Проверяем существование Issue с названием {issueName}")
+    public void checkIssueWithSpecificName (String issueName) {
+        $(withText(issueName)).should(Condition.visible);
+        Allure.getLifecycle().addAttachment(
+                "Исходники",
+                "text/html",
+                "html",
+                 WebDriverRunner.getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8)
+        );
+        takeScreenshot();
+    }
+    @Attachment(value = "Скриншот для всяких ребят", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot)WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+}
